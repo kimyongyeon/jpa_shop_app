b16dbf6bc8c1bd209a5e6707d94ae46361a91195
kimyongyeon <toyongyeon@gmail.com>
2021-12-07 23:22:23 +0900

    [feature] 타임리프 문법 공부 및 DTO 사용법 익히기

---
+++ src/main/java/com/shop/shop/controllers/ThymeleafExController.java
@@ -0,0 +1,65 @@
+package com.shop.shop.controllers;
+
+import com.shop.shop.controllers.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" +i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thyemeafEx/thymeleafEx03";
+    }
+
+    @GetMapping("/ex04")
+    public String thymeleafExample04(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" +i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thyemeafEx/thymeleafEx04";
+    }
+
+    @GetMapping("/ex05")
+    public String thymeleafExample05(Model model) {
+        return "thyemeafEx/thymeleafEx05";
+    }
+}
---
+++ src/main/java/com/shop/shop/controllers/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.shop.shop.controllers.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+    private Long id;
+    private String itemNm;
+    private Integer price;
+    private String itemDetail;
+    private String sellStatCd;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
